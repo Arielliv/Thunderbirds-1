50753a617c1006db0d61a8c0b53d43cd967f1366
AdiVeiszman <adiveiszman@gmail.com>
2022-04-21 19:17:01 +0300

    mergde 2 handleFile funcs and handled when file doesn't exists

--- game.cpp
+++ game.cpp
@@ -4,6 +4,7 @@ void Game::start() {
 	bool isEsc = false;
 	bool isWon = false;
 	bool isByFileName = false;
+	bool didReadFile = false;
 	int winningCounter = 0;
 	MenuChoice userChoice = MenuChoice::ShowSettings;
 
@@ -16,26 +17,31 @@ void Game::start() {
 		return;
 	}
 
-	if (userChoice == MenuChoice::OpenFileByNameWithColor || userChoice == MenuChoice::OpenFileByNameWithoutColor) {
-		this->handleFileGameWithName(userChoice == MenuChoice::OpenFileByNameWithColor);
-		isByFileName = true;
-	}
-
 	while (this->lives > 0 && winningCounter < 3 && !isEsc) {
-		if (!isByFileName) {
-			this->handleFileGameWithoutName(userChoice == MenuChoice::WithColor, winningCounter);
-		}
-		else {
+		if ((userChoice == MenuChoice::OpenFileByNameWithColor || userChoice == MenuChoice::OpenFileByNameWithoutColor) && !isByFileName) {
+			didReadFile = this->handleFileGame(userChoice == MenuChoice::OpenFileByNameWithColor, -1);
+			isByFileName = true;
 			winningCounter = 2;
 		}
-		this->gameBoard = this->presetBoard;
-		isWon = this->gameBoard.play(isEsc, this->lives);
-		if (!isWon && !isEsc) {
-			clear_screen();
-			this->lives--;
+		else if(userChoice == MenuChoice::WithColor){
+			didReadFile = this->handleFileGame(userChoice == MenuChoice::WithColor, winningCounter);
 		}
-		else if(!isEsc) {
-			winningCounter++;
+
+		if (didReadFile) {
+			this->gameBoard = this->presetBoard;
+			isWon = this->gameBoard.play(isEsc, this->lives);
+			if (!isWon && !isEsc) {
+				clear_screen();
+				this->lives--;
+			}
+			else if (!isEsc) {
+				winningCounter++;
+			}
+		}
+		else {
+			userChoice = this->menu.openMenu();
+			isByFileName = false;
+			winningCounter = 0;
 		}
 	}
 	if (!isEsc) {
@@ -51,29 +57,30 @@ void Game::start() {
 	}
 }
 
-void Game::handleFileGameWithName(bool isWithColor) {
+bool Game::handleFileGame(bool isWithColor, int fileNumber) {
 	int time;
 	GameFile fileGame;
 	int legendLocation;
 	int controlledShip;
 	int numOfBlocks;
 	std::string boardGame = "";
-	this->fileGame.openFile(this->menu.getFileName(), -1);
-	this->fileGame.readFile(time, controlledShip, boardGame, legendLocation, numOfBlocks);
-	this->fileGame.closeFile();
-	this->presetBoard = Board(isWithColor, time, (BoardCellType)controlledShip, boardGame, legendLocation, numOfBlocks);
-}
+	std::string fileName = "";
 
-void Game::handleFileGameWithoutName(bool isWithColor, int fileNumber) {
-	int time;
-	GameFile fileGame;
-	int legendLocation;
-	int controlledShip;
-	int numOfBlocks;
-	std::string boardGame = "";
-	this->fileGame.openFile("", fileNumber);
-	this->fileGame.readFile(time, controlledShip, boardGame, legendLocation, numOfBlocks);
-	this->fileGame.closeFile();
-	this->presetBoard = Board(isWithColor,time,(BoardCellType)controlledShip,boardGame, legendLocation,numOfBlocks);
+	if (fileNumber == -1) {
+		fileName = this->menu.getFileName();
+	}
+
+	if (this->fileGame.openFile(fileName, fileNumber)) {
+		this->fileGame.readFile(time, controlledShip, boardGame, legendLocation, numOfBlocks);
+		this->fileGame.closeFile();
+		this->presetBoard = Board(isWithColor, time, (BoardCellType)controlledShip, boardGame, legendLocation, numOfBlocks);
+		return true;
+	}
+	else {
+		gotoxy(25, 20);
+		std::cout << "File dosn't exists :(";
+		Sleep(1000);
+		return false;
+	}
 }
 
